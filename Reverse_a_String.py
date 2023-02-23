7377ab4249923036f9a5067677dea91bc7e324e4
Udit Narayan Ojha <125638067+unarayan280@users.noreply.github.com>
2023-02-23 09:07:42 +0530

    Add files via upload

---
+++ Reverse_a_String.py
@@ -0,0 +1,10 @@
+# Operating System List
+systems = ['Windows', 'macOS', 'Linux']
+print('Original List:', systems)
+
+# List Reverse
+systems.reverse()
+
+
+# updated list
+print('Updated List:', systems)
