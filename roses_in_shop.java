9f4da1b8e397a4d7457144f265dd88cf01a4eea6
Udit Narayan Ojha <125638067+unarayan280@users.noreply.github.com>
2023-04-01 07:22:34 +0530

    Add files via upload

---
+++ roses_in_shop.java
@@ -0,0 +1,160 @@
+import java.io.DataInputStream; 
+import java.io.FileInputStream; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.Scanner; 
+import java.util.StringTokenizer; 
+import java.util.HashMap;
+import java.util.Map;
+import java.math.BigInteger;
+public class Main 
+{ 
+    static class Reader 
+    { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead; 
+  
+        public Reader() 
+        { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public Reader(String file_name) throws IOException 
+        { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public String readLine() throws IOException 
+        { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) 
+            { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        } 
+  
+        public int nextInt() throws IOException 
+        { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do
+            { 
+                ret = ret * 10 + c - '0'; 
+            }  while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public long nextLong() throws IOException 
+        { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public double nextDouble() throws IOException 
+        { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+  
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (c == '.') 
+            { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        private void fillBuffer() throws IOException 
+        { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        } 
+  
+        private byte read() throws IOException 
+        { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        } 
+  
+        public void close() throws IOException 
+        { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+    } 
+  
+    public static void main(String[] args) throws IOException 
+    { 
+        Reader sc=new Reader(); 
+        int n = sc.nextInt(); 
+        int a[] = new int[n];
+ 
+        for(int i=0; i<n; i++) a[i] = sc.nextInt();
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+ 
+        prefix[0] = 1;
+        for(int i=1; i<n; i++) prefix[i] = a[i]>a[i-1]?prefix[i-1]+1:1;
+        suffix[n-1] = 1;
+        for(int i=n-2; i>=0; i--) suffix[i] = a[i]<a[i+1]?suffix[i+1]+1:1;
+ 
+        int ans = 1;
+        for(int i=0; i<n; i++){
+            if(prefix[i]>ans) ans=prefix[i];
+        }
+ 
+        for(int i=1; i<n-1; i++){
+            if(a[i-1]<a[i+1]){
+                ans = Math.max(ans, prefix[i-1]+suffix[i+1]);
+            }
+        }
+        System.out.println(ans);
+    } 
+}
